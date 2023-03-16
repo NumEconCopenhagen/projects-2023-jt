742d84d67c9fa7df05b4f3c90d5e015538636b9b
rdh702 <julianepruser@gmail.com>
2023-03-16 22:36:21 +0100

    ...

--- inauguralproject/JHouseholdSpecializationModel.py
+++ inauguralproject/JHouseholdSpecializationModel.py
@@ -56,7 +56,7 @@ class HouseholdSpecializationModelClass:
         if par.sigma == 0 :
             H = np.minimum(HM,HF)
         if par.sigma == 1 : 
-            H = HM**(1-par.sigma) * HF**par.sigma 
+            H = HM**(1-par.alpha) * HF**par.alpha
         else : 
             H = ((1- par.alpha)* HM**((par.sigma-1)/ par.sigma)) + par.alpha* HF**((par.sigma-1)/ par.sigma)**(par.sigma / par.sigma-1)
 
