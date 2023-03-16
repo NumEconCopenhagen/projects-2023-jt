00105e22c4da9706a70ea3263df7a2789b0effc7
ndc388 <ndc388@alumni.ku.dk>
2023-03-16 11:18:38 +0100

    .

--- inauguralproject/IP - test.py
+++ inauguralproject/IP - test.py
@@ -1,3 +1,17 @@
+#importing packages
+from types import SimpleNamespace
+
+import numpy as np
+from scipy import optimize
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Defining the class
 class household : 
-    def 
-        pass
+    def __init__(self):
+        
+        #creating namespaces
+        par = self.par = SimpleNamespace()
+        sol = self.sol = SimpleNamespace()
+        
