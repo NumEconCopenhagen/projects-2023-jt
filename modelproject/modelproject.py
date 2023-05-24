8e889aa1bb32ce9bacb7c2e98f47b54b6ab02d61
ndc388 <ndc388@alumni.ku.dk>
2023-05-24 21:54:53 +0200

    123

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -148,47 +148,9 @@ class SolowModelClass:
         hss_function(alpha,phi,delta,n,g,s_K,s_H)
 
         return 'The steady state for k is ', kss_function(alpha,phi,delta,n,g,s_K,s_H) ,'and the steady state for h is',  hss_function(alpha,phi,delta,n,g,s_K,s_H)
-   
-    def Nullclines(self, do_sim=False, do_plot=True, periods=100, steady_state=True):
-        par = self.par
-        sim = self.sim
-        periods = periods
 
-        if do_sim:
-            # Create the lambdified functions
-            ncht_expr = ((par.n + par.g + par.delta + par.n * par.g) / par.s_K) ** (1 / par.phi) * par.ktilde_t ** ((1 - par.alpha) / par.phi)
-            nckt_expr = (par.s_H/(par.n+par.g+par.delta+par.n*par.g))**(1/(1-par.phi))*par.ktilde_t**(par.alpha/(1-par.alpha))
-            ncht_func = sm.lambdify(par.ktilde_t, ncht_expr.subs({par.alpha: sim.alpha, par.phi: sim.phi, par.delta: sim.delta, par.n: sim.n, par.g: sim.g, par.s_K: sim.s_K}))
-            nckt_func = sm.lambdify(par.ktilde_t, nckt_expr.subs({par.alpha: sim.alpha, par.phi: sim.phi, par.delta: sim.delta, par.n: sim.n, par.g: sim.g, par.s_H: sim.s_H}))
-
-            # Evaluate the functions for different t_values
-            ktilde_vals = np.linspace(0, periods-1, periods)
-            ncht_vals = ncht_func(ktilde_vals)
-            htilde_vals = np.linspace(0, periods-1, periods)
-            nckt_vals = nckt_func(htilde_vals)
-
-            if do_plot:
-                plt.plot(ncht_vals, label="ht=0")
-                plt.plot(nckt_vals, label="kt=0")
-                plt.xlim(0, periods-1)
-                plt.ylim(0, periods-1)
-                plt.xlabel('ktilde')
-                plt.ylabel('htilde')
-                plt.title('Nullclines')
-
-            if steady_state:
-                ktilde_expr = ((par.s_K**(1-par.phi) * par.s_H**par.phi)/(par.n + par.g + par.delta + par.n*par.g))**(1/(1-par.phi-par.alpha))
-                htilde_expr = ((par.s_K**par.alpha * par.s_H**(1-par.alpha))/(par.n + par.g + par.delta + par.n*par.g))**(1/(1-par.phi-par.alpha))
-                ktilde_func = sm.lambdify([], [ktilde_expr.subs({par.phi: sim.phi, par.alpha: sim.alpha, par.s_K: sim.s_K, par.s_H: sim.s_H, par.n: sim.n, par.g: sim.g, par.delta: sim.delta})])
-                htilde_func = sm.lambdify([], [htilde_expr.subs({par.phi: sim.phi, par.alpha: sim.alpha, par.s_K: sim.s_K, par.s_H: sim.s_H, par.n: sim.n, par.g: sim.g, par.delta: sim.delta})])
-                ktilde_steady_state = ktilde_func()[0]
-                htilde_steady_state = htilde_func()[0]
-                print(f"ktilde in steady state: {ktilde_steady_state}, htilde in steady state: {htilde_steady_state}")
-
-                if do_plot:
-                    plt.plot(ktilde_steady_state, htilde_steady_state, 'ro', label='Steady State')
-                    
-    def Nullclines2(self, do_sim=False, do_plot=True, periods=500, steady_state=True):
+ 
+    def Nullclines(self, do_sim=False, do_plot=True, periods=500, steady_state=True):
         par = self.par
         sim = self.sim
         periods = periods
